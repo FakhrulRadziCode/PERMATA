cde6033057e2f1cc8bfb906c0e0294a6737cdb98
FakhrulRadziCode <fakhrulradzi200@gmail.com>
2022-05-25 17:13:51 +0800

    add session name

--- dashboard.php
+++ dashboard.php
@@ -82,7 +82,7 @@ if(!isset($_SESSION['userlogged']) || $_SESSION['userlogged'] !=1) {
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Welcome <?php $_SESSION['studentname'];?></a>
+          <a href="#" class="d-block"><?php echo $_SESSION['studentname'];?></a>
         </div>
       </div>
 
