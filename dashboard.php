fe860de36c88dd35094448e2b3712fa94122cbff
FakhrulRadziCode <fakhrulradzi200@gmail.com>
2022-05-25 13:42:23 +0800

    idk

--- dashboard.php
+++ dashboard.php
@@ -82,7 +82,7 @@ if(!isset($_SESSION['userlogged']) || $_SESSION['userlogged'] !=1) {
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">FAKHRUL RADZI BIN AHMAD</a>
+          <a href="#" class="d-block">Welcome <?php $_SESSION['studentname'];?></a>
         </div>
       </div>
 
